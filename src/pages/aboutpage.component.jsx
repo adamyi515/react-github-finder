1ecab6677f096091d3ba1150e1f9eeda2df7efa8
0mni-gear515 <adamyi515@gmail.com>
2022-02-21 18:04:43 -0600

    Update the aboutpage component and do a simple style on it.

--- src/pages/aboutpage.component.jsx
+++ src/pages/aboutpage.component.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import styles from './aboutpage.module.css';
+
 
 const AboutPage = () => {
     return(
-        <div>
-            <h1>About Page</h1>
+        <div className={`${styles['about-page']}`}>
+            <h1>About Github Finder</h1>
+            <p>
+                Github finder is a React Application that uses Context API and the Github APIs to search for Users within the Github website. 
+                It uses 2 endpoints to grab the users. One for grabbing a LIST of users based on the user's input and the other grabs back
+                one specific user information (object).
+            </p>
+            <h4>Version: 1.0.0</h4>
         </div>
     )
 }
+
 
 export default AboutPage;
